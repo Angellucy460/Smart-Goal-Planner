ba003fec516e7c623ad8f06693279d0ec2483e8a
Angellucy460 <angelliona38@gmail.com>
2025-07-20 18:44:11 +0300

    goals

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-import { useState } from 'react'
+import  { useEffect, useState} from "react";
+import GoalList from "./Components/GoalList";
+import GoalForm from "./Components/GoalForm"; 
+
+function App() {
+  const [goals, setGoals] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/goals")
+    .then(res => res.json())
+    .then(data => setGoals(data))
+  },[]);
+  return (
+    <div>
+      <h1>Goal Tracker</h1>
+      <GoalForm setGoals={setGoals}/>
+      <GoalList goals={goals} setGoals={setGoals}/>
+    </div>
+  )
+}
 
 
+export default App;
