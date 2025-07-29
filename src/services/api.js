52d8bd2f0271e37bfd26641edfeb127cb5f92ad5
Angellucy460 <angelliona38@gmail.com>
2025-07-30 00:02:29 +0300

    commited

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,19 @@
-const BASE = 'http://localhost:3000/goals';
+const BASE = 'https://dapper-tiramisu-929eaf.netlify.app';
 
 export const getGoals = () => fetch(BASE).then(res => res.json());
 
 export const createGoal = goal =>
-  fetch(BASE, {
+  fetch(`${BASE}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(goal)
   }).then(res => res.json());
 
 export const deleteGoal = id =>
-  fetch(`${BASE}/${id}`, { method: 'DELETE' });
+  fetch(`${BASE}/goals/${id}`, { method: 'DELETE' });
 
 export const updateGoal = (id, data) =>
-  fetch(`${BASE}/${id}`, {
+  fetch(`${BASE}/goals/${id}`, {
     method: 'PATCH',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
